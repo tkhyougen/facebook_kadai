17bdd73c9eec16e199c355517163563a071c56bf
tomohiko <illgalsubstance@yahoo.co.jp>
2020-01-08 15:06:10 +0000

    提出用に訂正

--- config/application.rb
+++ config/application.rb
@@ -3,10 +3,13 @@ require_relative 'boot'
 require 'rails/all'
 
 
+
 Bundler.require(*Rails.groups)
 
 module FacebookKadai
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
